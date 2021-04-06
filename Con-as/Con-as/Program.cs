5880d2c5f57a698278621fae4c6176332adb4e6f
as <>
2021-04-06 19:58:14 +0900

    project create

---
+++ Con-as/Con-as/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Con_as
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
